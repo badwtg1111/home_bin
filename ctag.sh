11ba275cf9bfa99719b51543e9fe60c8dda3c859
chenchunsheng <chenchunsheng@smartisan.cn>
2014-09-28 15:39:35 +0800

    modify ctag.sh for cctree

--- ctag.sh
+++ ctag.sh
@@ -6,5 +6,6 @@ echo $1
 
 find $1  -name "*.h" -o -name "*.c" -o -name "*.cc" -o -name "*.cpp" -o -name "*.java"   > cscope.files
 cscope -Rbkq -i cscope.files
+ccglue -S cscope.out -o cctree.out
 ctags -R --c++-kinds=+p --fields=+iaS --extra=+q  $1
 
