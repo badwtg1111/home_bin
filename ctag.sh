8ffc537527abc5a458c8150428fcb962fccc8066
chenchunsheng <chenchunsheng@smartisan.cn>
2014-10-28 11:39:14 +0800

    modify for ctags

--- ctag.sh
+++ ctag.sh
@@ -7,5 +7,7 @@ echo $1
 find $1  -name "*.h" -o -name "*.c" -o -name "*.cc" -o -name "*.cpp" -o -name "*.java"   > cscope.files
 cscope -Rbkq -i cscope.files
 ccglue -S cscope.out -o cctree.out
-ctags -R --c++-kinds=+p --fields=+iaS --extra=+q  $1
+
+find $1 -name "*.h" -o -name "*.c" -o -name "*.cpp" -o -name "*.cc" -o -name "*.m" -o -name "*.mm" -o -name "*.java" -o -name "*.py" -o -name "*.mk"  > src.files
+ctags -R --c++-kinds=+px --fields=+iaS --extra=+q -L src.files
 
