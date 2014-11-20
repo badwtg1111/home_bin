877ce2790f1f9119cdf16199ce70bebea515a306
chenchunsheng <chenchunsheng@smartisan.cn>
2014-11-20 14:02:52 +0800

    modify logcat

--- logc.sh
+++ logc.sh
@@ -3,6 +3,6 @@ packageName=$1
 logfile=$2
 
 pid=`adb shell ps | grep $packageName | awk '{print $2}'`
-adb logcat | grep --color=auto $pid|tee $logfile
+adb logcat -v threadtime | grep --color=auto $pid|tee $logfile
 
 
